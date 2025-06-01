7a4165220e9ec14fa914c7e20141e45da4b2e584
Kiaogiri <kiaogiri123@gmail.com>
2025-06-02 00:24:10 +0700

    Update ukk_kantin.sql

--- ukk_kantin.sql
+++ ukk_kantin.sql
@@ -1,42 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 01 Jun 2025 pada 19.02
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ukk_kantin`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `makanan`
---
-
 CREATE TABLE `makanan` (
   `id` int(11) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `makanan`
---
 
 INSERT INTO `makanan` (`id`, `nama`, `stok`, `harga`) VALUES
 (17, 'batagor', 22, 10000),
@@ -55,26 +22,6 @@ INSERT INTO `makanan` (`id`, `nama`, `stok`, `harga`) VALUES
 (30, 'air_mineral', 22, 3000),
 (31, 'pop_ice', 22, 6000),
 (32, 'jus_buah', 22, 8000);
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `makanan`
---
-ALTER TABLE `makanan`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `makanan`
---
-ALTER TABLE `makanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
